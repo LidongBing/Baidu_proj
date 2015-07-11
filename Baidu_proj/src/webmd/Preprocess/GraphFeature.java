50f622ecef74294afc897e7359aee9a22c8a9f0a
Lidong <binglidong@gmail.com>
2015-07-11 13:50:32 -0400

    GraphFeature

--- Baidu_proj/src/webmd/Preprocess/GraphFeature.java
+++ Baidu_proj/src/webmd/Preprocess/GraphFeature.java
@@ -5,6 +5,8 @@ package webmd.Preprocess;
  * Input: bow_context.tok_feat, the file contains the context of the sentence containing a list
  * Format: s_10000_8 <TAB> bowContext=the bowContext=rod 
  * 
+ * Input: webmd_export-2015-06-08T19-34-11_sentId_info.txt, 
+ * format: 6801022152812041277 <TAB> SIMVASTATIN <TAB> Zocor <TAB> Interactions
  * 
  */
 
