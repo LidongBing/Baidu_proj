bb17675b0f0c0731e62cc4b5daf7b74bbd44f921
iCrown <lingmingyang1@hotmail.com>
2015-07-12 14:40:52 -0400

    Seperate single multi relation

--- Baidu_proj/src/freebase/ExtractRelationSeed.java
+++ Baidu_proj/src/freebase/ExtractRelationSeed.java
@@ -10,20 +10,25 @@ public class ExtractRelationSeed {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-//		String factPath = "/remote/curtis/baidu/mingyanl/freebase/freebase-easy-14-04-14/facts.txt";
+		// String factPath =
+		// "/remote/curtis/baidu/mingyanl/freebase/freebase-easy-14-04-14/facts.txt";
 		String relativeOutPath = "../out/";
-		BufferedWriter bw = new BufferedWriter(new FileWriter(relativeOutPath + args[0].replaceAll(" ", "_")));
+		String relation = args[0].replaceAll(" ", "_").toLowerCase().replaceAll("[^a-zA-Z0-9_]", "");
+		BufferedWriter bw = new BufferedWriter(new FileWriter(relativeOutPath + relation));
 		BufferedReader stdInput = new BufferedReader(new InputStreamReader(System.in));
 		String line = null;
-		System.out.println(args[0]);
 		while ((line = stdInput.readLine()) != null) {
 			String[] tokens = line.split("\t");
 			if (tokens[1].equals(args[0])) {
-				System.out.println(line);
-				bw.write(tokens[0] + "\t" + tokens[2]+"\n");
+				// System.out.println(line);
+				bw.write("seed\t" + relation + "\t" + parse(tokens[0]) + "@" + parse(tokens[2]) + "\n");
 			}
 		}
 		bw.flush();
 		bw.close();
+	}
+
+	public static String parse(String input) {
+		return input.trim().replaceAll("\\s+", "_").toLowerCase();
 	}
 }
